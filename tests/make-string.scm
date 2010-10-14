9c35800c6c93c1431631e218ef101b03eccafc8d
Oleg Parashchenko <olpa@uucode.com>
2010-10-14 13:49:44 +0200

    Initial.

---
+++ tests/make-string.scm
@@ -0,0 +1,16 @@
+(display "[")
+(display (make-string 0)); empty string
+(display "]\n")
+
+(display "[")
+(display (make-string 5)); unspeficied, let's use space
+(display "]\n")
+
+(display "[")
+(display (make-string 0 #\q)); again, empty string
+(display "]\n")
+
+(display "[")
+(display (make-string 5 #\q)); "qqqqq"
+(display "]\n")
+
